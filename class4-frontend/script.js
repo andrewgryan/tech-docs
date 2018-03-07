cd12b7742304d07e00c3bf8abca8dd2ef0709d76
andrew.ryan <andrew.g.m.ryan@gmail.com>
2018-03-07 15:10:40 +0000

    changed title to be h1 content

--- class4-frontend/script.js
+++ class4-frontend/script.js
@@ -21,4 +21,10 @@ window.onload = function() {
         ul.append(li);
     }
     toc.append(ul);
+
+    // Set title to h1 content
+    let h1 = document.body.getElementsByTagName("h1");
+    if (h1.length == 1) {
+        document.title = h1[0].innerHTML;
+    }
 };
